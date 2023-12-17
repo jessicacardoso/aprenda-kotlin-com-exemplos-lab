7a69844230ea47cc6a665f5aa514f47662c96d76
Jessica Cardoso <jessicasousa.pc@gmail.com>
2023-12-17 15:06:11 -0300

    :recycle: refactor: ajustes iniciais no desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,51 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(var nome: String, var email: String, val id: Int){
+    override fun equals(other: Any?) =
+        other is Usuario && other.id == this.id
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+    override fun hashCode() = id
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel = Nivel.BASICO)
+
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val user1 = Usuario("Alex", "alex@email.com", 1)
+    val user2 = Usuario("Ana", "ana@email.com", 2)
+    val user3 = Usuario("João", "joao@email.com", 3)
+
+    val cursoKotlin = ConteudoEducacional("Curso de Kotlin", 480, Nivel.AVANCADO)
+    val cursoAndroid = ConteudoEducacional("Curso de Android", 120, Nivel.INTERMEDIARIO)
+    val cursoKotlinAndroid = ConteudoEducacional("Curso de Kotlin Android", 60, Nivel.BASICO)
+
+    val formacaoAndroid = Formacao("Formação Android", listOf(cursoKotlin, cursoAndroid, cursoKotlinAndroid))
+    val formacaoKotlin = Formacao("Formação Kotlin", listOf(cursoKotlin, cursoKotlinAndroid))
+
+    formacaoAndroid.matricular(user1)
+    formacaoAndroid.matricular(user2)
+    formacaoKotlin.matricular(user1)
+    formacaoKotlin.matricular(user3)
+
+    println("Usuários matriculados na formação Android:")
+    for (usuario in formacaoAndroid.inscritos) {
+        println(usuario)
+    }
+    println()
+
+    println("Usuários matriculados na formação Kotlin:")
+    for (usuario in formacaoKotlin.inscritos) {
+        println(usuario)
+    }
+
 }
